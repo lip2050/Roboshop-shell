f2aa463bbdb568e2b0c8c2f038b9217afa5ed70c
lip2050 <lip2050@users.noreply.github.com>
2023-06-06 02:07:05 -0400

    Roboshop Bash scripting

--- Redis.sh
+++ Redis.sh
@@ -1,14 +1,14 @@
 echo -e "\e[32mInstall Redis Repos\e[0m"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>/tmp/roboshop.log
 
 echo -e "\e[32mEnable Redis 6 Version \e[0m"
-yum module enable redis:remi-6.2 -y
+yum module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
 
 echo -e "\e[32mInstall Redis\e[0m"
-yum install redis -y
+yum install redis -y &>>/tmp/roboshop.log
 
 echo -e "\e[32mUpdate Redis Listen address\e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
 
 echo -e "\e[32mStart Redis Service\e[0m"
 systemctl enable redis
