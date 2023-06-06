b1bd16ae4fed1079bc1305d0c8bf16861ec7f007
lip2050 <lip2050@users.noreply.github.com>
2023-06-06 03:39:08 -0400

    Roboshop Bash scripting

--- shipping.sh
+++ shipping.sh
@@ -1,35 +1,35 @@
 echo -e "\e[32m Install Maven\e[0m"
-yum install maven -y
+yum install maven -y &>>/tmp/roboshop.log
 
 echo -e "\e[32m Add Application user \e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[32m Create Application Directory \e[0m"
-rm -rf /app
+rm -rf /app &>>/tmp/roboshop.log
 mkdir /app
 
 echo -e "\e[32m Download Application Content\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
 
 echo -e "\e[32m Extract application Content\e[0m"
-unzip /tmp/shipping.zip
-cd /app
+unzip /tmp/shipping.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
 
 echo -e "\e[32m Download Maven Dependencies\e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
+mvn clean package    &>>/tmp/roboshop.log
+mv target/shipping-1.0.jar shipping.jar  &>>/tmp/roboshop.log
 
 echo -e "\e[32m Setup SystemD File\e[0m"
-cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service  &>>/tmp/roboshop.log
 
 echo -e "\e[32m Install MySQL Client\e[0m"
-yum install mysql -y
+yum install mysql -y &>>/tmp/roboshop.log
 
 echo -e "\e[32m Load Schema\e[0m"
-mysql -h mysql-dev.lipdevopspro.site -uroot -pRoboShop@1 < /app/schema/shipping.sql
+mysql -h mysql-dev.lipdevopspro.site -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/roboshop.log
 
 echo -e "\e[32m Start Shipping Service\e[0m"
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+systemctl daemon-reload  &>>/tmp/roboshop.log
+systemctl enable shipping  &>>/tmp/roboshop.log
+systemctl restart shipping  &>>/tmp/roboshop.log
