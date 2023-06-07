d11f0991c3899b6fbe5922d9de935f63822d1875
lip2050 <lip2050@users.noreply.github.com>
2023-06-06 20:22:49 -0400

    Dispatch Bash scripting

--- catalogue.sh
+++ catalogue.sh
@@ -15,23 +15,23 @@ rm -rf ${app_path} &>>$log_file
 mkdir ${app_path}
 
 echo -e "${color}Download Application Content${nocolor}"
-curl -o /tmp/component.zip https://roboshop-artifacts.s3.amazonaws.com/component.zip &>>$log_file
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
 cd ${app_path}
 
 echo -e "${color}Extract Application Content${nocolor}"
-unzip /tmp/component.zip &>>$log_file
+unzip /tmp/${component}.zip &>>$log_file
 cd ${app_path}
 
 echo -e "${color} Install Nodejs Dependencies${nocolor}"
 npm install &>>$log_file
 
 echo -e "${color}Setup SystemD Service${nocolor}"
-cp /home/centos/roboshop-shell/component.service /etc/systemd/system/component.service &>>$log_file
+cp /home/centos/roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>$log_file
 
-echo -e "${color}Start component Service ${nocolor}"
+echo -e "${color}Start ${component} Service ${nocolor}"
 systemctl daemon-reload
-systemctl enable component
-systemctl start component
+systemctl enable ${component}
+systemctl start ${component}
 
 echo -e "${color}Copy MongoDB Repo file ${nocolor}"
 cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>$log_file
@@ -40,4 +40,4 @@ echo -e "${color}Install mongodb client${nocolor}"
 yum install mongodb-org-shell -y &>>$log_file
 
 echo -e "${color}Load Schema ${nocolor}"
-mongo --host mongodb-dev.lipdevopspro.site </app/schema/component.js &>>$log_file
+mongo --host mongodb-dev.lipdevopspro.site </${app_path}/schema/${component}.js &>>$log_file
